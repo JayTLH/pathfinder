c259ee3789627267c6b97fcbc1402cb6e74a07cc
jason tlh <jasonjdh94@gmail.com>
2020-07-31 21:54:57 -0400

    added repo link, preparing to style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Pathfinder from './components/Pathfinder.js';
 import './App.scss';
+import github from './assets/github.png';
 
 export default function App(props) {
   return (
     <div className="app">
       <Pathfinder />
+      <a className="link" href="https://github.com/JayTLH/pathfinder" target="_blank" rel="noopener noreferrer">
+            <img className="link__icon" src={github} alt="github" />
+          </a>
     </div>
   )
 };
