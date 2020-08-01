a552c1ce0c85fc7dde78a7e08a06ee5299af64da
jason tlh <jasonjdh94@gmail.com>
2020-08-01 19:57:20 -0400

    added logo

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Pathfinder from './components/Pathfinder.js';
 import './App.scss';
 import github from './assets/github.png';
+import logo from './assets/logo.png';
 
 export default function App(props) {
   return (
     <div className="app">
+      <img className="logo" src={logo} alt="logo" />
       <Pathfinder />
       <a className="link" href="https://github.com/JayTLH/pathfinder" target="_blank" rel="noopener noreferrer">
             <img className="link__icon" src={github} alt="github" />
